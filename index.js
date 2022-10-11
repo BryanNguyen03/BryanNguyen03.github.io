f1160901c6541ab0e57e38596089a63a5e2df00a
Bryan <bryannguyen051012@gmail.com>
2022-10-11 19:33:12 -0400

    added images and fixed formatting

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ window.addEventListener('scroll', fixNav)
 function fixNav() {
     if(window.scrollY > navbar.offsetHeight + 150){
         navbar.classList.add('active')
+        iconsOnWhite()
     }
     else {
         navbar.classList.remove('active')
+        iconsOnBlack()
     }
 }
 
-//Expanding Cards for About Me Page
+//Functions for changing github and linkedin Icon whenever nav bar is active
+function iconsOnBlack() {
+    document.getElementById("gitIcon").src = "images/githubIcon.png"
+    document.getElementById("linkedinIcon").src = "images/LinkedinIcon.png"
+}
+
+function iconsOnWhite() {
+    document.getElementById("gitIcon").src = "images/githubOnWhite.png"
+    document.getElementById("linkedinIcon").src = "images/linkedinOnWhite.png"
+}
+
+//Expanding Cards for projects page
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
@@ -26,5 +39,4 @@ function removeActiveClasses(){
         panel.classList.remove('active')
     })
 }
-
 
